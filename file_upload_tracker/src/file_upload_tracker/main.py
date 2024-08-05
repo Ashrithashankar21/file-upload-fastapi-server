4c5a9f7a3302d61a86675336f067d4ddc2978d47
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-08-05 19:40:19 +0530

    feat: src: track csv files
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- file_upload_tracker/src/file_upload_tracker/main.py
+++ file_upload_tracker/src/file_upload_tracker/main.py
@@ -1,8 +1,49 @@
-from fastapi import FastAPI
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from threading import Timer
 
-app = FastAPI()
+CSV_FILE_EXTENSION = ".csv"
 
 
-@app.get("/")
-def print_hi():
-    return {"message": "Hi!"}
+class DebouncedEventHandler(FileSystemEventHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.event_timers = {}
+
+    def _debounce(self, event_type, event):
+        if event.src_path in self.event_timers:
+            self.event_timers[event.src_path].cancel()
+
+        def handle_event():
+            if event.src_path.endswith(CSV_FILE_EXTENSION):
+                print(f"File {event_type}: {event.src_path}")
+            del self.event_timers[event.src_path]
+
+        timer = Timer(0.5, handle_event)
+        self.event_timers[event.src_path] = timer
+        timer.start()
+
+    def on_modified(self, event):
+        self._debounce("modified", event)
+
+    def on_created(self, event):
+        self._debounce("created", event)
+
+    def on_deleted(self, event):
+        self._debounce("deleted", event)
+
+
+if __name__ == "__main__":
+    path = "C:/Users/ashritha.shankar/Downloads"
+    event_handler = DebouncedEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
