82cb6c36b3d3291e7f481a0419f787fe3ab4b841
Ashritha Shankar <ashritha.shankar@solitontech.com>
2024-08-06 15:10:50 +0530

    feat: src: clean the code
    Signed-off-by: Ashritha Shankar <ashritha.shankar@solitontech.com>

--- file_upload_tracker/tests/conftest.py
+++ file_upload_tracker/tests/conftest.py
@@ -3,7 +3,5 @@ import os
 
 sys.path.insert(
     0,
-    os.path.abspath(
-        os.path.join(os.path.dirname(__file__), "../src/file_upload_tracker")
-    ),
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")),
 )
